94b161fb8fdbc254ab198292998464165081e75b
HectorSanchezLuque <92037326+HectorSanchezLuque@users.noreply.github.com>
2022-12-08 16:09:51 +0100

    Add files via upload

---
+++ AE2_Sockets/src/es/florida/AE2_Sockets/Cliente.java
@@ -0,0 +1,10 @@
+package es.florida.AE2_Sockets;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AE2_Sockets/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RxRos
+ *
+ */
+module AE2_Sockets {
+}
