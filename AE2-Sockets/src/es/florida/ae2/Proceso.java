a6f5d39e42c046bc079221749b1c99644a191442
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-12-15 11:57:41 +0100

    Cositas de servidor

--- AE2-Sockets/src/es/florida/ae2/Proceso.java
+++ AE2-Sockets/src/es/florida/ae2/Proceso.java
@@ -7,62 +7,116 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Proceso implements Runnable {
 	BufferedReader bfr;
 	PrintWriter pw;
 	Socket socket;
-	static Boolean turno;
-
-	public Boolean getPrimerTurno() {
-		return turno;
-	}
-
-	public static void setTurno(Boolean primerTurno) {
-		turno = primerTurno;
-	}
 
 	public Proceso(Socket socket) {
 		this.socket = socket;
-		turno = false;
+		
 	}
 
-	static void parImpar(Socket socket, BufferedReader bfr, PrintWriter pw, String resp) {
+	static boolean parImpar(int resp) {
 		int num = (int) (Math.random() * 10 + 1);
-		String result;
+		int result;
 		
 		if(num % 2 == 0) {
 			
-			result ="par";
+			result = 0;
 		}else {
-			result="impar";
+			result = 1;
 		}
 		
-		if(result.equals(resp.toLowerCase())) {
+		if (result == resp)  {
 			
-			setTurno(true);
+			return(true);
 			
+		}else {
+			return false;
 		}
-	
-		
 		
 	}
+	//comprueba el ganador indicando si son la 'X' o 'O' y si no devuelve 'continue'
+	 static String compGanador(String [] board)
+	    {
+	        for (int a = 0; a < 8; a++) {
+	            String line = null;
+	 
+	            switch (a) {
+	            case 0:
+	                line = board[0] + board[1] + board[2];
+	                break;
+	            case 1:
+	                line = board[3] + board[4] + board[5];
+	                break;
+	            case 2:
+	                line = board[6] + board[7] + board[8];
+	                break;
+	            case 3:
+	                line = board[0] + board[3] + board[6];
+	                break;
+	            case 4:
+	                line = board[1] + board[4] + board[7];
+	                break;
+	            case 5:
+	                line = board[2] + board[5] + board[8];
+	                break;
+	            case 6:
+	                line = board[0] + board[4] + board[8];
+	                break;
+	            case 7:
+	                line = board[2] + board[4] + board[6];
+	                break;
+	            }
+	            //For X winner
+	            if (line.equals("XXX")) {
+	                return "X";
+	            }
+	             
+	            // For O winner
+	            else if (line.equals("OOO")) {
+	                return "O";
+	            }
+	        }
+	        return "continue";
+	    }
+	
 
 	public void run() {
 		// TODO Auto-generated method stub
-		String[] tabla;
+		String client;
+		boolean acierto = false;
+		String [] tabla;
 		try {
 			InputStream is = socket.getInputStream();
 
 			InputStreamReader isr = new InputStreamReader(is);
 			bfr = new BufferedReader(isr);
 
-			OutputStream os = socket.getOutputStream();
-			pw = new PrintWriter(os);
+//			OutputStream os = socket.getOutputStream();
+//			pw = new PrintWriter(os);
 
-			tabla = bfr.readLine().split("");
+			client = bfr.readLine();
+			System.err.println("Servidor>> Recivido Info cliente: "+client);
+			
+			if(client.length() == 1) {
+				
+				 acierto = parImpar(Integer.parseInt(client));
+				 
+				 System.out.println("SERVIDOR >> turno del cliente es: "+acierto);
+				
+			}
+			//Ver el tema de la tabla si se espera que le envies la tabla o realiza el el primer turno
+			
+			
+			
+			
+			
 
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println("SERVIDOR >>> Error.");
 		}
--- AE2-Sockets/src/es/florida/ae2/Servidor.java
+++ AE2-Sockets/src/es/florida/ae2/Servidor.java
@@ -1,15 +1,29 @@
 package es.florida.ae2;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method 
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method
 
-		
-		
-		
-
+		System.err.println("SERVIDOR >>> Arranca el servidor, espera peticion");
+		ServerSocket socketEscucha = null;
+		try {
+			socketEscucha = new ServerSocket(1234);
+		} catch (IOException e) {
+			System.err.println("SERVIDOR >>> Error");
+			return;
+		}
+		while (true) {
+			Socket conexion = socketEscucha.accept();
+			System.err.println("SERVIDOR >>> Conexion recibida --> Lanza hilo de la tabla");
+			Proceso p = new Proceso(conexion);
+			Thread hilo = new Thread(p);
+			hilo.start();
+		}
 	}
 
 }
