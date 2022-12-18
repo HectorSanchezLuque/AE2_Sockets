a28089f3616f319447122bb14dbc6d4970775d71
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-12-18 17:55:17 +0100

    pauseo

--- AE2-Sockets/src/es/florida/ae2/Proceso.java
+++ AE2-Sockets/src/es/florida/ae2/Proceso.java
@@ -7,7 +7,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Proceso implements Runnable {
 	BufferedReader bfr;
@@ -43,6 +45,14 @@ public class Proceso implements Runnable {
 	// comprueba el ganador indicando si son la 'X' o 'O' y si no devuelve
 	// 'continue'
 	static String compGanador(String[] board) {
+	   
+		 
+		 String list = board[0]+board[1]+board[2]+board[3]+board[4]+board[5]+board[6]+board[7]+board[8];
+		if( !list.contains(" ")) {
+			return "terminado";
+		}
+		 
+		
 		for (int a = 0; a < 8; a++) {
 			String line = null;
 
@@ -110,6 +120,7 @@ public class Proceso implements Runnable {
 		// TODO Auto-generated method stub
 		String result;
 		String client;
+		int ronda = 0;
 		boolean acierto = false;
 		String[] tabla = new String[] {" "," "," "," "," "," "," "," "," "};
 		try {
@@ -140,9 +151,12 @@ public class Proceso implements Runnable {
 					pw.flush();
 				}
 				while(true) {
-					
+					ronda++;
+					System.out.println("Ronda: "+ronda);
 					System.err.println("SERVIDOR >> ESPERA DE TABLA");
 					client = bfr.readLine();
+
+					System.out.println(client);
 					tabla = client.split("");
 					result = compGanador(tabla);
 					System.err.println("SERVIDOR >> el resultado es: "+ result);
@@ -152,22 +166,25 @@ public class Proceso implements Runnable {
 						tabla = insert_O(tabla).split("");
 						result = compGanador(tabla);
 						
-						if(result.equals("X") || result.equals("O")) {
-							
-							pw.print(result);
+						if(result.equals("X") || result.equals("O") || result.equals("terminado")) {
+							System.out.println("Ganador es "+result);
+							pw.print(tabla[0]+tabla[1]+tabla[2]+tabla[3]+tabla[4]+tabla[5]+tabla[6]+tabla[7]+tabla[8]+ result+"\n");
 							pw.flush();
-						}{
-							
+			                Thread.currentThread().interrupt();
+
+						}else{
+							System.out.println("Ganador es nadie");
 							pw.print(tabla[0]+tabla[1]+tabla[2]+tabla[3]+tabla[4]+tabla[5]+tabla[6]+tabla[7]+tabla[8]+ "\n");
 							pw.flush();
 						}
 						
 						
 					}else {
-						
-						pw.print(result);
+						System.out.println("Ganador es "+result);
+						pw.print(tabla[0]+tabla[1]+tabla[2]+tabla[3]+tabla[4]+tabla[5]+tabla[6]+tabla[7]+tabla[8]+result+"\n");
 						pw.flush();
-						
+		                Thread.currentThread().interrupt();
+
 					}
 					
 					
@@ -176,9 +193,6 @@ public class Proceso implements Runnable {
 				
 
 			}
-			// Ver el tema de la tabla si se espera que le envies la tabla o realiza el el
-			// primer turno
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println("SERVIDOR >>> Error.");
